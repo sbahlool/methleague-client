d59679b69bd202e89405d620c59b19eeaef157de
sbahlool <saud.bahlool@hotmail.com>
2024-08-23 23:17:16 +0300

    style mobile table

--- src/pages/Table.jsx
+++ src/pages/Table.jsx
@@ -5,6 +5,7 @@ import '../index.css'
 const PremierLeagueStandings = () => {
   const [standings, setStandings] = useState([])
   const [error, setError] = useState(null)
+  const [isMobile, setIsMobile] = useState(false)
 
   useEffect(() => {
     const getStandings = async () => {
@@ -18,6 +19,17 @@ const PremierLeagueStandings = () => {
     }
 
     getStandings()
+
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 640) // Adjust based on your mobile breakpoint
+    }
+
+    handleResize()
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
   }, [])
 
   if (error) {
@@ -47,57 +59,65 @@ const PremierLeagueStandings = () => {
                     <th className="px-2 py-2 text-left">Last 5</th>
                   </tr>
                 </thead>
-                <tbody className="bg-black-800 divide-y divide-gray-600 text-xs">
-                  {standings.map((team) => (
-                    <tr key={team.team.id} className="bg-white bg-opacity-20">
-                      <td className="px-2 py-2">{team.position}</td>
-                      <td className="flex items-center px-2 py-2 whitespace-nowrap">
-                        <img
-                          className="w-6 h-6"
-                          src={team.team.crest}
-                          alt={team.team.shortName}
-                        />
-                        <span className="ml-2 font-medium truncate">
-                          {team.team.shortName}
-                        </span>
-                      </td>
-                      <td className="px-2 py-2">{team.playedGames}</td>
-                      <td className="px-2 py-2">{team.won}</td>
-                      <td className="px-2 py-2">{team.draw}</td>
-                      <td className="px-2 py-2">{team.lost}</td>
-                      <td className="px-2 py-2">{team.goalsFor}</td>
-                      <td className="px-2 py-2">{team.goalsAgainst}</td>
-                      <td className="px-2 py-2">{team.goalDifference}</td>
-                      <td className="px-2 py-2">{team.points}</td>
-                      <td className="flex items-center px-2 py-2">
-                        {team.form ? (
-                          team.form.split(',').map((result, index) => (
-                            <svg
-                              key={index}
-                              className={`w-4 h-4 fill-current ${
-                                result === 'W'
-                                  ? 'text-green-600'
-                                  : result === 'L'
-                                  ? 'text-red-600'
-                                  : 'text-gray-400'
-                              }`}
-                              xmlns="http://www.w3.org/2000/svg"
-                              viewBox="0 0 20 20"
-                              fill="currentColor"
-                            >
-                              <path
-                                fillRule="evenodd"
-                                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                                clipRule="evenodd"
-                              />
-                            </svg>
-                          ))
-                        ) : (
-                          <span className="text-gray-400"></span>
-                        )}
-                      </td>
-                    </tr>
-                  ))}
+                <tbody className="bg-black-800 divide-y text-xs">
+                  {standings.map((team, index) => {
+                    const position = index + 1 // Ensure position is calculated correctly
+                    return (
+                      <tr
+                        key={team.team.id}
+                        className={`bg-white bg-opacity-20 ${getPositionClass(
+                          position
+                        )}`}
+                      >
+                        <td className="px-2 py-2">{position}</td>
+                        <td className="flex items-center px-2 py-2 whitespace-nowrap">
+                          <img
+                            className="w-6 h-6"
+                            src={team.team.crest}
+                            alt={team.team.shortName}
+                          />
+                          <span className="ml-2 font-medium truncate">
+                            {isMobile ? team.team.tla : team.team.shortName}
+                          </span>
+                        </td>
+                        <td className="px-2 py-2">{team.playedGames}</td>
+                        <td className="px-2 py-2">{team.won}</td>
+                        <td className="px-2 py-2">{team.draw}</td>
+                        <td className="px-2 py-2">{team.lost}</td>
+                        <td className="px-2 py-2">{team.goalsFor}</td>
+                        <td className="px-2 py-2">{team.goalsAgainst}</td>
+                        <td className="px-2 py-2">{team.goalDifference}</td>
+                        <td className="px-2 py-2">{team.points}</td>
+                        <td className="flex items-center px-2 py-2">
+                          {team.form ? (
+                            team.form.split(',').map((result, i) => (
+                              <svg
+                                key={i}
+                                className={`w-4 h-4 fill-current ${
+                                  result === 'W'
+                                    ? 'text-green-600'
+                                    : result === 'L'
+                                    ? 'text-red-600'
+                                    : 'text-gray-400'
+                                }`}
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                              >
+                                <path
+                                  fillRule="evenodd"
+                                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                                  clipRule="evenodd"
+                                />
+                              </svg>
+                            ))
+                          ) : (
+                            <span className="text-gray-400"></span>
+                          )}
+                        </td>
+                      </tr>
+                    )
+                  })}
                 </tbody>
               </table>
             </div>
@@ -106,6 +126,21 @@ const PremierLeagueStandings = () => {
       </div>
     </div>
   )
+}
+
+const getPositionClass = (position) => {
+  switch (position) {
+    case 1:
+      return 'border-b-2 border-green-300'
+    case 5:
+      return 'border-t-4 border-b-2 border-green-300'
+    case 6:
+      return 'border-t-4 border-b-2 border-green-300'
+    case 18:
+      return 'border-t-4 border-red-300'
+    default:
+      return ''
+  }
 }
 
 export default PremierLeagueStandings
