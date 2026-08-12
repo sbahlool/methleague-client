27e092156b2b1e128dbe1070b185d33708694db4
sbahlool <saud.bahlool@hotmail.com>
2026-08-12 23:14:22 +0300

    styling changes

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+                                                                                                                                           import React, { useState, useEffect } from 'react'
 import { addMatch, getMatches, MatchRequest, MatchResponse, Scores, updateMatchScores } from '../services/Match'
 import { getTeams, TeamResponse } from '../services/Auth'
 import '../style/schedule.css'
